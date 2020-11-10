3bd44d151c1f4a0f7528e65649f92d8ef475f95f
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-10 20:12:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function (data) {
+    return {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+let createEmployeeRecords = function (dataSet) {
+    return dataSet.map(function(data) {
+        return createEmployeeRecord(data)
+    })
+}
+
+let createTimeInEvent = function (timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+
+let createTimeOutEvent = function (timeStamp) {
+    let [date, hour] = timeStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this 
+}
+
+let hoursWorkedOnDate = function(soughtDate){
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+
+let wagesEarnedOnDate = function (workDate) {
+    let earnings = hoursWorkedOnDate.call(this, workDate) * this.payPerHour
+    return parseFloat(earnings.toString())
+}
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +75,20 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+// solution after this
+
+
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function (collection, firstNameString) {
+    return collection.find(function(name) {
+        return name.firstName === firstNameString
+    })
 }
